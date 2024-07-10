04c63e1599a7d43c1d445c7119df85e4ffc9b5b5
Treeceanu <stefancopaceanu@gmail.com>
2024-07-11 02:02:54 +0300

    second Commit

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance= axios.create({
-    baseURL:'http://localhost:8001'
+    baseURL:'https://harmoniq-backend-gk2i.onrender.com/'
 })
 
 export default instance;
